4d6ee4bc922617bf06a888e77f8b56d03f3275a7
Keanna Salter <keannasalter91@gmail.com>
2020-02-27 00:06:01 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,4 +7,4 @@ class Anagram
   
   def match(array_of_words)
     array_of_words.select.do |element|
-      (@word.split("").sort) ==(element.split)
+      (@word.split("").sort) ==(element.split(""))
